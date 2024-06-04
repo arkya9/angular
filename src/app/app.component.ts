86e20bb1f53bc008d6bc21d127ebab3cf54af4e6
arkya9 <arkoapple@gmail.com>
2024-06-04 17:45:44 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { ChildComponentComponent } from './components/child-component/child-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,UserProfileComponent,ChildComponentComponent],
+ templateUrl: './app.component.html',
+ //template:"<h1>test ts</h1>",
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+   reciveddatafromchild(e:string){
+    console.log(e);
+
+  }
+  reciveddatafromchild2(e:string){
+    console.log(e);
+
+  }
+  //title = 'product1';
+}
